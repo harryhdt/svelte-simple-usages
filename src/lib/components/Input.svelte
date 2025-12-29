ea2b133bd5a9e617676d9d504ac24025a450c6a9
Harry Hidayat <harryhdtofficial@gmail.com>
2025-12-29 13:22:49 +0800

    feat: component input

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { HTMLInputAttributes } from 'svelte/elements';
+
+	interface InputProps extends HTMLInputAttributes {
+		use?: (node: HTMLElement) => void;
+	}
+	const { ...props }: InputProps = $props();
+
+	function action(node: HTMLElement) {
+		props.use?.(node);
+	}
+</script>
+
+<input use:action {...props} />
