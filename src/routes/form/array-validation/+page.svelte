efabd1cb3a692a614a17617c8d87db659b2b4ae8
Harry Hidayat <harryhdtofficial@gmail.com>
2026-03-18 12:20:31 +0800

    feat: enhance array validation form with new fields and update functionality

--- src/routes/form/array-validation/+page.svelte
+++ src/routes/form/array-validation/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Breadcrumb from '$lib/components/Breadcrumb.svelte';
 	import z from 'zod';
-	import { Trash2Icon } from '@lucide/svelte';
+	import { PencilIcon, Trash2Icon } from '@lucide/svelte';
 	import { useFormControl } from 'svelte-simple-form';
 	import { standardSchemaValidator } from '@svelte-simple-form/validators/standard-schema';
 	// import { useFormControl } from '$lib/form/form.svelte';
@@ -15,6 +15,7 @@
 		contacts: z
 			.array(
 				z.object({
+					id: z.string().min(1),
 					label: z.string().min(1),
 					value: z.string().min(1)
 				})
@@ -24,8 +25,8 @@
 		encrypt2: z.array(
 			z.array(
 				z.object({
-					name: z.string().optional(),
-					code: z.number().min(200)
+					name: z.string().min(3),
+					code: z.number().min(400)
 				})
 			)
 		)
@@ -38,7 +39,7 @@
 			code: [],
 			contacts: undefined,
 			encrypt: [[]],
-			encrypt2: [[]]
+			encrypt2: []
 		},
 		validator: standardSchemaValidator(schema),
 		onSubmit: async (values) => {
@@ -48,7 +49,7 @@
 			success = Math.random() > 0.5 ? true : false;
 		}
 	});
-	form.setData('encrypt2', [[{ code: 0 }]]);
+	form.setData('encrypt2', [[{ name: '300P', code: 300 }]]);
 </script>
 
 <div class="p-2">
@@ -101,9 +102,7 @@
 						type="button"
 						class="btn btn-outline-success"
 						onclick={() => {
-							form.arrayAdd('code', Math.floor(Math.random() * 1000), undefined, {
-								shouldValidate: true
-							});
+							form.arrayAdd('code', Math.floor(Math.random() * 1000), undefined);
 						}}
 					>
 						Add
@@ -111,16 +110,13 @@
 					<button
 						type="button"
 						class="btn btn-outline-danger"
-						onclick={() =>
-							form.arrayRemove('code', form.data.code.length - 1, {
-								shouldValidate: true
-							})}
+						onclick={() => form.arrayRemove('code', form.data.code.length - 1)}
 					>
 						Delete
 					</button>
 				</div>
 				<div>
-					{#each form.data.code as code, i}
+					{#each form.data.code as code, i (i + '-' + code)}
 						<span class="badge me-1 {form.errors[`code.${i}`] ? 'bg-danger' : 'bg-success'}">
 							{code}
 						</span>
@@ -129,13 +125,25 @@
 					{/each}
 				</div>
 				{#if form.data.code.length > 3}
-					<div class="mt-2">
+					<div class="mt-2 flex items-center gap-2">
 						<button
 							onclick={() => form.arraySwap('code', 0, 3)}
 							type="button"
 							class="btn btn-sm btn-light border"
 						>
 							Swap 1 & 4
+						</button>
+						<button
+							onclick={() =>
+								form.arrayUpdateBy(
+									'code',
+									(item) => item === form.data.code[1],
+									Math.floor(Math.random() * 1000)
+								)}
+							type="button"
+							class="btn btn-sm btn-light border"
+						>
+							Update 2
 						</button>
 					</div>
 				{/if}
@@ -152,6 +160,7 @@
 						onclick={() => {
 							if (!form.data.contacts) form.setData('contacts', []);
 							form.arrayAdd('contacts', {
+								id: crypto.randomUUID(),
 								label: 'Data ' + ((form.data.contacts?.length || 0) + 1),
 								value: ''
 							});
@@ -161,7 +170,7 @@
 					</button>
 				</div>
 				<div class="">
-					{#each form.data.contacts as contact, i}
+					{#each form.data.contacts as contact, i (contact.id + '-' + i)}
 						{@const errorsLabel = form.errors[`contacts.${i}.label`]}
 						{@const errorsValue = form.errors[`contacts.${i}.value`]}
 						<div class="input-group" style="margin-top: -1px;">
@@ -172,12 +181,30 @@
 								placeholder="Label"
 								use:control={`contacts.${i}.label`}
 							/>
+
 							<input
 								type="text"
 								class="form-control {errorsValue ? 'is-invalid' : ''}"
 								placeholder="Value"
 								use:control={`contacts.${i}.value`}
 							/>
+							<button
+								type="button"
+								class="btn btn-outline-danger"
+								onclick={() => {
+									form.arrayUpdateBy(
+										'contacts',
+										(item) => item.id === contact.id,
+										(v) => ({
+											...v,
+											label: v.label + '!',
+											value: v.value + '!'
+										})
+									);
+								}}
+							>
+								<PencilIcon size={16} />
+							</button>
 							<button
 								type="button"
 								class="btn btn-outline-danger"
@@ -213,16 +240,93 @@
 				{/if}
 			</div>
 
-			<button type="submit" class="btn btn-primary" disabled={form.isSubmitting || !form.isValid}>
-				{#if form.isSubmitting}
-					<div class="spinner-border text-white spinner-border-sm" role="status">
-						<span class="visually-hidden">Loading...</span>
-					</div>
-				{:else}
-					Submit
-				{/if}
-			</button>
-			<button type="button" class="btn btn-danger" onclick={() => form.reset()}> Reset </button>
+			<div class="mb-3">
+				<label for="contacts" class="form-label">Encrypt2</label>
+				<div class="d-flex gap-2 mb-2">
+					<button
+						type="button"
+						class="btn btn-outline-success"
+						onclick={() => {
+							if (!form.data.encrypt2) form.setData('encrypt2', []);
+							const code = Math.floor(Math.random() * 1000);
+							form.arrayAdd('encrypt2', [{ code, name: code + 'P' }]);
+						}}
+					>
+						Add
+					</button>
+				</div>
+				<div class="">
+					{#each form.data.encrypt2 as encrypt, i (i)}
+						{@const errorsName = form.errors[`encrypt2.${i}.0.name`]}
+						{@const errorsCode = form.errors[`encrypt2.${i}.0.code`]}
+						<div class="input-group" style="margin-top: -1px;">
+							<input
+								type="text"
+								class="form-control {errorsName ? 'is-invalid' : ''}"
+								placeholder="Name"
+								use:control={`encrypt2.${i}.0.name`}
+							/>
+							<input
+								type="number"
+								class="form-control {errorsCode ? 'is-invalid' : ''}"
+								placeholder="Code"
+								use:control={{
+									field: `encrypt2.${i}.0.code`,
+									valueAsNumber: true
+								}}
+							/>
+							<button
+								type="button"
+								class="btn btn-outline-danger"
+								onclick={() => {
+									const newCode = Math.floor(Math.random() * 1000);
+									form.arrayUpdateBy('encrypt2', (item) => item[0].code === encrypt[0].code, [
+										{ code: newCode, name: newCode + 'P' }
+									]);
+								}}
+							>
+								<PencilIcon size={16} />
+							</button>
+							<button
+								type="button"
+								class="btn btn-outline-danger"
+								onclick={() =>
+									form.arrayRemoveBy('encrypt2', (item) => item[0].code === encrypt[0].code)}
+							>
+								<Trash2Icon size={16} />
+							</button>
+						</div>
+						{#if errorsCode || errorsName}
+							<div class="invalid-feedback d-block mb-2">
+								{#if errorsName}
+									Name: {errorsName?.join(', ')}
+								{/if}
+								{#if errorsName && errorsCode}
+									<br />
+								{/if}
+								{#if errorsCode}
+									Code: {errorsCode?.join(', ')}
+								{/if}
+							</div>
+						{/if}
+					{:else}
+						<p class="text-muted">No encrypt2 data yet</p>
+					{/each}
+				</div>
+			</div>
+
+			<div>
+				<button type="submit" class="btn btn-primary" disabled={form.isSubmitting || !form.isValid}>
+					{#if form.isSubmitting}
+						<div class="spinner-border text-white spinner-border-sm" role="status">
+							<span class="visually-hidden">Loading...</span>
+						</div>
+					{:else}
+						Submit
+					{/if}
+				</button>
+				<button type="button" class="btn btn-danger" onclick={() => form.reset()}> Reset </button>
+			</div>
 		</form>
 		<div class="col bg-light py-2" style="white-space: pre; font-size: 14px;">
 			<code>
