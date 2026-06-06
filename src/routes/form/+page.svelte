ffee482a7c5ad6da9a73ead7898850031d69253a
Harry Hidayat <harryhdtofficial@gmail.com>
2026-06-06 13:59:59 +0800

    fix: update links to use resolve for routing in form page

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { resolve } from '$app/paths';
 	import Breadcrumb from '$lib/components/Breadcrumb.svelte';
 </script>
 
@@ -18,31 +19,34 @@
 	<h1 class="fs-3 fw-semibold mt-2 mb-2">Svelte Simple Form Usage</h1>
 	<ol>
 		<li>
-			<a href="/form/basic">Basic</a>
+			<a href={resolve('/form/basic')}>Basic</a>
 		</li>
 		<li>
-			<a href="/form/basic-validation">Basic Validation</a>
+			<a href={resolve('/form/basic-validation')}>Basic Validation</a>
 		</li>
 		<li>
-			<a href="/form/basic-default-values-with-validation">Basic Default Values with Validation</a>
+			<a href={resolve('/form/basic-default-values-with-validation')}>Basic Default Values with Validation</a>
 		</li>
 		<li>
-			<a href="/form/nested-validation">Nested Validation</a>
+			<a href={resolve('/form/nested-validation')}>Nested Validation</a>
 		</li>
 		<li>
-			<a href="/form/file-upload-validation">File Upload Validation</a>
+			<a href={resolve('/form/file-upload-validation')}>File Upload Validation</a>
 		</li>
 		<li>
-			<a href="/form/array-validation">Array Validation</a>
+			<a href={resolve('/form/array-validation')}>Array Validation</a>
 		</li>
 		<li>
-			<a href="/form/password-validation">Password Validation</a>
+			<a href={resolve('/form/password-validation')}>Password Validation</a>
 		</li>
 		<li>
-			<a href="/form/all-input-validation">All input Validation</a>
+			<a href={resolve('/form/all-input-validation')}>All input Validation</a>
 		</li>
 		<li>
-			<a href="/form/component-validation">Component Validation</a>
+			<a href={resolve('/form/component-validation')}>Component Validation</a>
+		</li>
+		<li>
+			<a href={resolve('/form/async-validation')}>Async Validation</a>
 		</li>
 	</ol>
 </div>
